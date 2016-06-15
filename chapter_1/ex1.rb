90366d7e7a81574a3ae80f4fd3bf66687c43cb6b
Misikir <misikirmehari@gmail.com>
2016-06-15 13:41:19 -0600

    Answers to chapter 1 [#120732859]

---
+++ chapter_1/ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World"
+puts "Hello Again"
+puts "I like typing this"
+puts "this is fun "
+puts "yay! prinitng"
+puts "I'd much rather you 'not'"
+puts 'I "said" do not touch this'
+puts "I am learning ruby the hard way"
