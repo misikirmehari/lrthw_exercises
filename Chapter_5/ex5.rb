a59e174ed9c86a93ea702c090b33b9c01f348ece
Misikir <misikirmehari@gmail.com>
2016-06-17 12:46:02 -0600

    Answers to chapter 5 [#120732867]

---
+++ Chapter_5/ex5.rb
@@ -0,0 +1,40 @@
+
+#Assigning name to the variable name
+name = 'Misikir A. Mehari'
+
+#Assigning age to the variable age
+age = 26
+
+#Assigning height to the variable height in cm
+height_cm = 173
+
+#Assigning height in inches to the variable height_in
+height_in = height_cm *  0.393701
+
+#Assigning weight to the variable weight in lbs
+weight = 162
+
+#Assigning weight to the variable weight_kilo by converting from lbs
+weight_kilo = 162 * 0.453592
+
+
+#Assigning eye color  to the variable my_eyes
+eyes = 'Brown'
+
+#Assigning teeth color to the variable my_teeth
+teeth = 'white'
+
+#Assigning hair color to the variable my_hair
+hair = 'Black'
+
+puts "Let's talk about #{name}."
+puts "He's #{height_cm} centimeters tall."
+puts "He's height in inches is #{height_in}"
+puts "He's weight in Kilograms is #{weight_kilo}"
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+
+puts "If I add #{age}, #{height_in}, and #{weight} I get #{age + height_in + weight}."
