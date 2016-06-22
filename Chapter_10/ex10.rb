4598cbdd86b119744d47e90946f9c9885242e6e7
Misikir <misikirmehari@gmail.com>
2016-06-22 11:13:45 -0600

    Answers to Chapter 10 [#120732877]

---
+++ Chapter_10/ex10.rb
@@ -0,0 +1,20 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+puts "\\This is test line 1"
+puts "\'This is test line 2"
+puts "\a This is test line 3"
+
